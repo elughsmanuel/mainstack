70b8965a10d7f587898a536d0aba87e730bb1927
Emmanuel Elugbaju <elugbajuemmanueltobi@gmail.com>
2023-11-23 00:28:11 +0100

    first commit: server is listening...

---
+++ src/app.ts
@@ -0,0 +1,53 @@
+import express from 'express';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+import http from "http";
+import { ReasonPhrases, StatusCodes } from 'http-status-codes';
+
+const app = express();
+const host = process.env.HOST || 'localhost';
+const port = Number(process.env.PORT || 8000);
+const httpServer = http.createServer(app);
+
+app.get('/', (req, res) => {
+    return  res.status(StatusCodes.OK).json({
+        success: true,
+        data: `${ReasonPhrases.OK} : Homepage`,
+    });
+});
+
+app.get('/api', (req, res) => {
+    return  res.status(StatusCodes.OK).json({
+        success: true,
+        data: `${ReasonPhrases.OK} : API`,
+    });
+});
+
+app.get('/api/v1', (req, res) => {
+    return  res.status(StatusCodes.OK).json({
+        success: true,
+        data: `${ReasonPhrases.OK} : API - v1`,
+    });
+});
+
+app.all('*', (req, res) => {
+    return res.status(StatusCodes.NOT_FOUND).json({
+        success: false,
+        data: `Can't find ${req.originalUrl} on this server.`,
+    });
+});
+
+const server = async () => {
+    try {
+        httpServer.listen(port, host, () => {
+            console.log(`🌟 🛠️  [SERVER] - Server is listening on http://${host}:${port}`);
+        });
+    }
+    catch(error) {
+        console.log(`🔥  [SERVER] - Error starting the server`, error);
+    }
+};
+
+server();
